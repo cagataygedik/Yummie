8d367598246df0079cdf74c924646072c58019a7
cagatay <cagatay.gedik@icloud.com>
2022-12-09 13:18:01 +0300

    Initial Commit

---
+++ Yummie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 9.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
