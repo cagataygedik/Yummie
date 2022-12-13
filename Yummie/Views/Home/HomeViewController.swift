52cd510f4eab200d6e9c1f9b65e45dc86bc3a714
cagatay <cagatay.gedik@icloud.com>
2022-12-13 20:41:40 +0300

    added Homepage to storyboard.

---
+++ Yummie/Views/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 13.12.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+}
