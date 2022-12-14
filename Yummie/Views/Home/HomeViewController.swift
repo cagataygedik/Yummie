2d5c7841434af82b404108baf226e25e626274ed
cagatay <cagatay.gedik@icloud.com>
2022-12-14 23:16:41 +0300

    added some dummy data and collection view functions.

--- Yummie/Views/Home/HomeViewController.swift
+++ Yummie/Views/Home/HomeViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    
+    var categories: [DishCategory] = [
+        .init(id: "id1", name: "Dish", image: "https://picsum.photos/200/300"),
+        .init(id: "id1", name: "Dish 2", image: "https://picsum.photos/200/300"),
+        .init(id: "id1", name: "Dish 3", image: "https://picsum.photos/200/300"),
+        .init(id: "id1", name: "Dish 4", image: "https://picsum.photos/200/300"),
+        .init(id: "id1", name: "Dish 5", image: "https://picsum.photos/200/300")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        registerCells()
 
+    }
+    
+    private func registerCells() {
+        categoryCollectionView.register(UINib(nibName: CategoryCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.identifier, for: indexPath) as! CategoryCollectionViewCell
+        cell.setup(category: categories[indexPath.row])
+        return cell
     }
 }
