64017c7cce149d1b495322a318fbe100e06f8d6a
cagatay <cagatay.gedik@icloud.com>
2022-12-14 23:00:51 +0300

    added Dish Category to the homepage.

---
+++ Yummie/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 14.12.2022.
+//
+
+import UIKit
+import Kingfisher
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: CategoryCollectionViewCell.self)
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    
+    func setup(category: DishCategory) {
+        categoryTitleLabel.text = category.name
+        categoryImageView.kf.setImage(with: category.image.asUrl)
+    }
+}
---
+++ Yummie/Models/DishCategory.swift
@@ -0,0 +1,14 @@
+//
+//  DishCategory.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 14.12.2022.
+//
+
+import Foundation
+
+struct DishCategory {
+    let id: String
+    let name: String
+    let image: String
+}
