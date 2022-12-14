21bdcae2da9b87602be73f7f1b70f53a259d9d03
cagatay <cagatay.gedik@icloud.com>
2022-12-14 23:10:22 +0300

    custom card view for dish categories in homepage.

---
+++ Yummie/CustomViews/CardView/CardView.swift
@@ -0,0 +1,28 @@
+//
+//  CardView.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 14.12.2022.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialSetup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialSetup()
+    }
+    
+    private func initialSetup() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.cornerRadius = 10
+        layer.shadowOpacity = 0.1
+    }
+}
