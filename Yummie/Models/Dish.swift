55f09d3ef419792a0bc1d83f8419c8fb1474b2b0
cagatay <cagatay.gedik@icloud.com>
2022-12-16 23:04:09 +0300

    Created new model for Popular dishes.

---
+++ Yummie/Models/Dish.swift
@@ -0,0 +1,20 @@
+//
+//  Dish.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 15.12.2022.
+//
+
+import Foundation
+
+struct Dish {
+    let id: String?
+    let name: String?
+    let description: String?
+    let image: String?
+    let calories: Int?
+    
+    var formattedCalories: String {
+        return "\(calories ?? 0) calories"
+    }
+}
