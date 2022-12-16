082dbe76192e84bc43b9afadc0e934465d3c5db6
cagatay <cagatay.gedik@icloud.com>
2022-12-16 23:20:54 +0300

    Created new CollectionView for Popular Dishes.

---
+++ Yummie/CustomViews/CollectionViewCells/DishPortraitCollectionViewCell/DishPortraitCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  DishPortraitCollectionViewCell.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 14.12.2022.
+//
+
+import UIKit
+
+class DishPortraitCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: DishPortraitCollectionViewCell.self)
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dishImageView: UIImageView!
+    @IBOutlet weak var caloriesLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func setup(dish: Dish) {
+        titleLabel.text = dish.name
+        dishImageView.kf.setImage(with: dish.image?.asUrl)
+        caloriesLabel.text = dish.formattedCalories
+        descriptionLabel.text = dish.description
+    }
+}
