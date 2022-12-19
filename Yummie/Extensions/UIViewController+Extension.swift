e77e0fe5e612b54da5df9b28f8314fb1d677ad1b
cagatay <cagatay.gedik@icloud.com>
2022-12-19 22:58:29 +0300

    finished DishDetail page.

---
+++ Yummie/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Extension.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 19.12.2022.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static func instantiate() -> Self {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        return storyboard.instantiateViewController(withIdentifier: identifier) as! Self
+    }
+}
--- Yummie/Views/Home/HomeViewController.swift
+++ Yummie/Views/Home/HomeViewController.swift
@@ -89,4 +89,14 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
             return UICollectionViewCell()
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == categoryCollectionView {
+            
+        } else {
+            let controller = DishDetailViewController.instantiate()
+            controller.dish = collectionView == popularCollectionView ? populars[indexPath.row] : specials[indexPath.row]
+            navigationController?.pushViewController(controller, animated: true)
+        }
+    }
 }
