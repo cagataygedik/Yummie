35611f01cedd571738cfbf5fcef4d1fa1e487a76
cagatay <cagatay.gedik@icloud.com>
2022-12-20 23:41:55 +0300

    added new model for ListOrders Page.

---
+++ Yummie/Models/Order.swift
@@ -0,0 +1,14 @@
+//
+//  Order.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 20.12.2022.
+//
+
+import Foundation
+
+struct Order {
+    let id: String?
+    let name: String?
+    let dish: Dish?
+}
