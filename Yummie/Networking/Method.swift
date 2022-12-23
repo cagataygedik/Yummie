67be62d4966f2d2d54d05f058cf36a5bcfcbc32f
cagatay <cagatay.gedik@icloud.com>
2022-12-23 12:43:53 +0300

    Created Networking folder and created Methods off-type enum.

---
+++ Yummie/Networking/Method.swift
@@ -0,0 +1,16 @@
+//
+//  Method.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 23.12.2022.
+//
+
+import Foundation
+
+enum Method: String {
+    
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
