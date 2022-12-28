a707f9831159a6fb020d8f2a401fe0f81568e566
cagatay <cagatay.gedik@icloud.com>
2022-12-28 15:20:25 +0300

    Created ApiResponse structure.

---
+++ Yummie/Networking/ApiResponse.swift
@@ -0,0 +1,15 @@
+//
+//  ApiResponse.swift
+//  Yummie
+//
+//  Created by Celil Çağatay Gedik on 27.12.2022.
+//
+
+import Foundation
+
+struct ApiResponse<T: Decodable>: Decodable {
+    let status: Int
+    let message: String?
+    let data: T?
+    let error: String?
+}
